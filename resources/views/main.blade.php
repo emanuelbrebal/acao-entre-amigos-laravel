1bf5f20276f5a92ad2b100fe68e2c18c68785f5e
Emanuel <emanuel.brebal@seplag.al.gov.br>
2024-12-27 11:13:45 -0300

    created the main page

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -1,6 +1,59 @@
 @extends('layouts.app')
 @section('title', 'Página inicial')
-
+<link rel="stylesheet" href="{{ asset('css/layouts/mainInicial.css') }}">
 @section('content')
-    <h1>Bem vindo a minha aplicação</h1>
+    <div class="d-flex main-search">
+        <h1 class="main-text">Campanhas ativas</h1>
+        <input type="text"  name="" id="" class="search-bar">
+        <input type="button" value="Pesquisar" class="search-button">
+    </div>
+
+    <section class="rifas-section">
+        {{-- @foreach ($rifas as $rifa)
+        @endforeach --}}
+        <div class="rifa-card">
+            <img src="{{asset('img/time_1000.jpg')}}" alt="" class="img-card-rifa">
+            <h3>instituicao->nome</h3>
+            <p>Valor do bilhete: R$ preco_numero</p>
+            <p>Data do sorteio: data_sorteio</p>
+            <a href="" class="search-button -more">Saiba +</a>
+        </div>
+        <div class="rifa-card">
+            <img src="{{asset('img/time_1000.jpg')}}" alt="" class="img-card-rifa">
+            <h3>instituicao->nome</h3>
+            <p>Valor do bilhete: R$ preco_numero</p>
+            <p>Data do sorteio: data_sorteio</p>
+            <a href="" class="search-button -more">Saiba +</a>
+        </div>
+        <div class="rifa-card">
+            <img src="{{asset('img/time_1000.jpg')}}" alt="" class="img-card-rifa">
+            <h3>instituicao->nome</h3>
+            <p>Valor do bilhete: R$ preco_numero</p>
+            <p>Data do sorteio: data_sorteio</p>
+            <a href="" class="search-button -more">Saiba +</a>
+        </div>
+        <div class="rifa-card">
+            <img src="{{asset('img/time_1000.jpg')}}" alt="" class="img-card-rifa">
+            <h3>instituicao->nome</h3>
+            <p>Valor do bilhete: R$ preco_numero</p>
+            <p>Data do sorteio: data_sorteio</p>
+            <a href="" class="search-button -more">Saiba +</a>
+        </div>
+        <div class="rifa-card">
+            <img src="{{asset('img/time_1000.jpg')}}" alt="" class="img-card-rifa">
+            <h3>instituicao->nome</h3>
+            <p>Valor do bilhete: R$ preco_numero</p>
+            <p>Data do sorteio: data_sorteio</p>
+            <a href="" class="search-button -more">Saiba +</a>
+        </div>
+        <div class="rifa-card">
+            <img src="{{asset('img/time_1000.jpg')}}" alt="" class="img-card-rifa">
+            <h3>instituicao->nome</h3>
+            <p>Valor do bilhete: R$ preco_numero</p>
+            <p>Data do sorteio: data_sorteio</p>
+            <a href="" class="search-button -more">Saiba +</a>
+        </div>
+
+    </section>
+
 @endsection
