9c5bb0ff0e7fa6d7866c4f8bdced8352a9cb9d0c
Emanuel <emanuel.brebal@seplag.al.gov.br>
2024-12-27 11:13:45 -0300

    created the login screen

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,30 +1,62 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>@yield('title', 'Minha Aplicação')</title>
-    <!-- Inclua seus estilos aqui -->
+    <title>@yield('title')</title>
+
+    <link rel="shortcut icon" href="{{asset('img/rifa_icon.png')}}" type="image/x-icon">
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&family=Roboto:ital,wght@0,300;0,400;0,500;0,700;1,400;1,500;1,700&display=swap"
+        rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset('css/layouts/reset.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/layouts/color.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/layouts/header.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/layouts/footer.css') }}">
 </head>
+
 <body>
-    <!-- Cabeçalho -->
     <header>
-        <nav>
-            <a href="">Home</a>
-            <a href="">Sobre</a>
-            <a href="">Contato</a>
+        <nav class="navbar nav">
+            <div class="navbar-brand">
+                <img class="icon-header" src="{{ asset('img/rifa_icon.png') }}" alt="Desenho de uma urna e uma mão, que está tirando uma rifa sorteada.">
+                <p class="navbar-brand-text"><strong>Ação Entre Amigos</strong></p>
+            </div>
+
+            <ul class="navigation">
+                <a class="navigation-item" href="">Participar</a>
+                <a class="navigation-item" href="">Sobre</a>
+                <a class="navigation-item" href="">Bem vindo(a)
+                    <svg class="login-icon" xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 448 512"><!--!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.-->
+                        <path
+                            d="M224 256A128 128 0 1 0 224 0a128 128 0 1 0 0 256zm-45.7 48C79.8 304 0 383.8 0 482.3C0 498.7 13.3 512 29.7 512l388.6 0c16.4 0 29.7-13.3 29.7-29.7C448 383.8 368.2 304 269.7 304l-91.4 0z" />
+                    </svg>
+                </a>
+
+            </ul>
         </nav>
     </header>
 
-    <!-- Conteúdo Principal -->
     <main>
         @yield('content')
     </main>
 
-    <!-- Rodapé -->
-    <footer>
-        <p>&copy; {{ date('Y') }} Minha Aplicação</p>
+    <footer class="footer">
+        <p class="footer-copy">&copy; {{ date('Y') }} Ação Entre Amigos</p>
+        <div>
+            <a href="https://github.com/emanuelbrebal" class="footer-link">github</a>
+            <a href="https://www.linkedin.com/in/emanuel-victor-brebal/" class="footer-link">linkedin</a>
+            <a href="" class="footer-link">contatos</a>
+            {{-- fazer um modal com todos os contatos --}}
+
+        </div>
     </footer>
+    <script src="https://kit.fontawesome.com/f6fb35c3c9.js" crossorigin="anonymous"></script>
 </body>
+
 </html>
